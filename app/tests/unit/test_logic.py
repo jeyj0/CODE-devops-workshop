5b4a508abbec90b6fd047f911cf8e13f83effa99
Jannis Jorre <jeyj0.plus@gmail.com>
2018-10-19 15:21:38 +0200

    Increased test-coverage for Calculator.mul

--- app/tests/unit/test_logic.py
+++ app/tests/unit/test_logic.py
@@ -7,5 +7,17 @@ class CalculatorTests(TestCase):
         calculator = Calculator()
         assert calculator.mul(5, 10) == 50
 
+    def test_mul_with_two_positive_numbers(self):
+        calculator = Calculator()
+        assert calculator.mul(3, 7) == 21
+
+    def test_mul_with_two_negative_numbers(self):
+        calculator = Calculator()
+        assert calculator.mul(-8, -4) == 32
+
+    def test_mul_with_one_positive_and_one_negative_number(self):
+        calculator = Calculator()
+        assert calculator.mul(6, -3) == -18
+
     def test_div(self):
         pass
